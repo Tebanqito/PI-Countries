711a8013c5a7fa55c7d5c3c6709e3daedb86692f
Tebanqito <yemesesteban@gmail.com>
2023-02-01 15:19:27 -0300

    createCountry eliminado

--- api/src/controllers/countryController.js
+++ api/src/controllers/countryController.js
@@ -1,11 +1,6 @@
 const { Country, Activity } = require("../db");
 const { Op } = require("sequelize");
 
-const createCountry = async (id, name, imgFlag, continent, capital, subRegion, area, poblacion) => {
-    const newCountry = await Country.create({ id, name, imgFlag, continent, capital, subRegion, area, poblacion });
-    return newCountry;
-};
-
 const getCountryById = async id => {
     const country = await Country.findByPk(id, { include: [Activity] });
     return country;
