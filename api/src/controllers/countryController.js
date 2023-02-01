2574f16aea09644cff38b398eae12542e9de1950
Tebanqito <yemesesteban@gmail.com>
2023-02-01 14:46:21 -0300

    findAllCountries

--- api/src/controllers/countryController.js
+++ api/src/controllers/countryController.js
@@ -2,7 +2,7 @@ const { Country, Activity } = require("../db");
 const { Op } = require("sequelize");
 
 const createCountry = async (id, name, imgFlag, continent, capital, subRegion, area, poblacion) => {
-    const newCountry = await Country.create({id, name, imgFlag, continent, capital, subRegion, area, poblacion });
+    const newCountry = await Country.create({ id, name, imgFlag, continent, capital, subRegion, area, poblacion });
     return newCountry;
 };
 
@@ -13,11 +13,28 @@ const getCountryById = async id => {
 
 const findAllCountriesByName = async name => {
     const countries = await Country.findAll({ where: { name: { [Op.iLike]: '%' + name + '%' } } });
-    return countries; 
+    return countries;
+}
+
+const findAllCountries = async () => {
+    const countries = await Country.findAll({
+        attributes: ["id", "name", "imgFlag", "continent", "capital", "subRegion", "area", "poblacion"],
+        include: [
+            {
+                model: Activity,
+                attributes: ["name", "difficulty", "duration", "season"],
+                through: {
+                    attributes: []
+                }
+            }
+        ]
+    });
+    return countries;
 }
 
 module.exports = {
     createCountry,
     getCountryById,
-    findAllCountriesByName
+    findAllCountriesByName,
+    findAllCountries
 };
