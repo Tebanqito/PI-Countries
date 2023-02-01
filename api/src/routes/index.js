f33c5458861757988ca925e52aca4bd43fc3f80d
Tebanqito <yemesesteban@gmail.com>
2023-02-01 15:33:10 -0300

    countryRouter funcionando en index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-// const countryRouter = require("./countryRouter");
+const countryRouter = require("./countryRouter");
 // const touristActivity = require("./touristActivityRouter");
 const axios = require("axios");
 const { Country, Activity } = require("../db");
@@ -9,86 +9,90 @@ const { Op } = require("sequelize");
 
 const router = Router();
 
+router.use("/countries", countryRouter);
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get("/countries", async (req, res) => {
-    const { name } = req.query;
+// router.get("/countries", async (req, res) => {
+//     const { name } = req.query;
 
-    try {
-        console.log("estoy en /countries");
-        if (name) {
-            const countries = await Country.findAll({ where: { name: { [Op.iLike]: '%' + name + '%' } } });
-            if (countries) return res.status(200).json(countries);
-            throw new Error(`No se ah encontrado el pais ${name} en la base de datos.`);
-        }
-        const countries = await Country.findAll({
-            attributes: ["id", "name", "imgFlag", "continent", "capital", "subRegion", "area", "poblacion"],
-            include: [
-                {
-                    model: Activity,
-                    attributes: ["name", "difficulty", "duration", "season"],
-                    through: {
-                        attributes: []
-                    }
-                }
-            ]
-        });
+//     try {
+//         console.log("estoy en /countries");
+//         if (name) {
+//             const countries = await Country.findAll({ where: { name: { [Op.iLike]: '%' + name + '%' } } });
+//             if (countries) return res.status(200).json(countries);
+//             throw new Error(`No se ah encontrado el pais ${name} en la base de datos.`);
+//         }
+//         const countries = await Country.findAll({
+//             attributes: ["id", "name", "imgFlag", "continent", "capital", "subRegion", "area", "poblacion"],
+//             include: [
+//                 {
+//                     model: Activity,
+//                     attributes: ["name", "difficulty", "duration", "season"],
+//                     through: {
+//                         attributes: []
+//                     }
+//                 }
+//             ]
+//         });
 
-        if (!countries.length) {
-            await axios.get("https://restcountries.com/v3/all")
-                .then(res => {
-                    const countries = res.data;
+//         if (!countries.length) {
+//             await axios.get("https://restcountries.com/v3/all")
+//                 .then(res => {
+//                     const countries = res.data;
 
-                    countries.forEach(async element => {
-                        await Country.create({
-                            id: element.cca3,
-                            name: element.name.official ? element.name.official : element.name.common,
-                            imgFlag: element.flags ? element.flags[0] : element.flag,
-                            continent: element.region ? element.region : element.continents[0],
-                            capital: element.capital ? element.capital[0] : "No posee capital",
-                            subRegion: element.subregion,
-                            area: element.area,
-                            poblacion: element.population
-                        });
-                    });
-                });
+//                     countries.forEach(async element => {
+//                         await Country.create({
+//                             id: element.cca3,
+//                             name: element.name.official ? element.name.official : element.name.common,
+//                             imgFlag: element.flags ? element.flags[0] : element.flag,
+//                             continent: element.region ? element.region : element.continents[0],
+//                             capital: element.capital ? element.capital[0] : "No posee capital",
+//                             subRegion: element.subregion,
+//                             area: element.area,
+//                             poblacion: element.population
+//                         });
+//                     });
+//                 });
 
-            return res.status(200).json({ status: "Paises cargados en la BDD." });
-        }
+//             return res.status(200).json({ status: "Paises cargados en la BDD." });
+//         }
 
-        return res.status(200).json(countries);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+//         return res.status(200).json(countries);
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// });
 
-router.get("/countriesActivity/:id", async (req, res) => {
-    const { id } = req.params;
+// router.get("/countriesActivity/:id", async (req, res) => {
+//     const { id } = req.params;
 
-    try {
-        console.log("estoy en /countriesActivity/:id");
-        const activity = await Activity.findByPk(id);
-        const countries = await activity.getCountries();
-        if (!countries.length) throw new Error("No se han encontrado paises con tal actividad");
-        res.status(200).json(countries);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+//     try {
+//         console.log("estoy en /countriesActivity/:id");
+//         const activity = await Activity.findByPk(id);
+//         const countries = await activity.getCountries();
+//         if (!countries.length) throw new Error("No se han encontrado paises con tal actividad");
+//         res.status(200).json(countries);
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// });
 
-router.get("/countries/:id", async (req, res) => {
-    const { id } = req.params;
+// router.get("/countries/:id", async (req, res) => {
+//     const { id } = req.params;
 
-    try {
-        console.log("estoy en /countries/:id");
-        const country = await Country.findByPk(id, { include: [Activity] });
-        if (!country) throw new Error("Pais no encontrado");
-        res.status(200).json(country);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+//     try {
+//         console.log("estoy en /countries/:id");
+//         const country = await Country.findByPk(id, { include: [Activity] });
+//         if (!country) throw new Error("Pais no encontrado");
+//         res.status(200).json(country);
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// });
+
+// ******************************************************************
 
 // router.post("/activities", async (req, res) => {
 //     const { countryId, name, difficulty, duration, season } = req.body;
