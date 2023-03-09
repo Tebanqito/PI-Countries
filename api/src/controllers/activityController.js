67101cc62ea2d987b5c7a95943f0b473cdbb4407
Tebanqito <yemesesteban@gmail.com>
2023-03-09 16:05:41 -0300

    CRUD activity

--- api/src/controllers/activityController.js
+++ api/src/controllers/activityController.js
@@ -1,7 +1,63 @@
 const { Activity } = require("../db");
 
-const getActivities = async () => await Activity.findAll();
+const createActivity = async (name, difficulty, duration, season) => {
+  const activity = await Activity.create({
+    name: name,
+    difficulty: difficulty,
+    duration: duration,
+    season: season,
+  });
+  return activity;
+};
+
+const getActivities = async () => {
+  const activities = await Activity.findAll();
+  return activities;
+};
+
+const getActivityById = async (id) => {
+  const activity = await Activity.findByPk(id);
+  return activity;
+};
+
+const getActivityByName = async (name) => {
+  const activity = await Activity.findOne({
+    where: { name: { [Op.like]: `%${name}%` } },
+  });
+  return activity;
+};
+
+const getActivitiesBySeason = async (season) => {
+  const activities = await Activity.findAll({ where: { season: season } });
+  return activities;
+};
+
+const getActivitiesByDifficulty = async (difficulty) => {
+  const activities = await Activity.findAll({
+    where: { difficulty: { [Op.eq]: difficulty } },
+  });
+  return activities;
+};
+
+const updateActivityById = async (id, attributes) => {
+  await Activity.update(attributes, { where: { id: id } });
+  const activityUpdated = await Activity.findByPk(id);
+  return activityUpdated;
+};
+
+const deleteActivityById = async (id) => {
+  const activityToDelete = await Activity.findByPk(id);
+  await Activity.destroy({ where: { id: id } });
+  return activityToDelete;
+};
 
 module.exports = {
-    getActivities
+  createActivity,
+  getActivities,
+  getActivitiesByDifficulty,
+  getActivityById,
+  getActivityByName,
+  getActivitiesBySeason,
+  updateActivityById,
+  deleteActivityById,
 };
