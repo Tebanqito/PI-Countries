c0d5ee6bedb988256e9af31f198f532b37034fe8
Tebanqito <yemesesteban@gmail.com>
2023-03-09 18:10:22 -0300

    CRUD de activity enrutado

--- api/src/routes/activityRouter.js
+++ api/src/routes/activityRouter.js
@@ -1,46 +1,157 @@
 const { Router } = require("express");
 const {
-    getActivities
+  getActivities,
+  createActivity,
+  getActivitiesByCountryId,
+  getActivitiesByCountryName,
+  getActivitiesByDifficulty,
+  getActivitiesBySeason,
+  getActivityByName,
+  updateActivityById,
+  deleteActivityById,
 } = require("../controllers/activityController");
-const { Country, Activity } = require("../db");
 const activityRouter = Router();
 
-activityRouter.post("/", async (req, res) => {
-    const { countriesId, name, difficulty, duration, season } = req.body;
-
-    try {
-        console.log("estoy en /activities");
-        if (![countriesId.length, name, difficulty, duration, season].every(Boolean)) {
-            throw new Error("Datos incompletos.");
-        }
-
-        const touristActivity = await Activity.create({
-            name: name,
-            difficulty: difficulty,
-            duration: duration,
-            season: season
-        });
-
-        for(let countryId of countriesId) {
-            let country = await Country.findByPk(countryId);
-            await country.addActivity(touristActivity);
-            await touristActivity.addCountry(country);
-        }
-
-        res.status(200).json({ activity: touristActivity });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+activityRouter.get("/getActivities", async (req, res) => {
+  try {
+    const touristActivities = await getActivities();
+    res.status(200).json(touristActivities);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-activityRouter.get("/getActivities", async (req, res) => {
-    try {
-        console.log("esoy en /getActivities");
-        const touristActivities = await getActivities();
-        res.status(200).json(touristActivities);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+activityRouter.get("/getActivitiesByDifficulty", async (req, res) => {
+  const { difficulty } = req.query;
+
+  try {
+    if (!difficulty) throw new Error("La dificultad esta indefinida.");
+
+    const activities = await getActivitiesByDifficulty(difficulty);
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.get("/getActivitiesBySeason", async (req, res) => {
+  const { season } = req.query;
+
+  try {
+    if (!season) throw new Error("La temporada esta indefinida.");
+
+    const activities = await getActivitiesBySeason(season);
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.get("/getActivityByName", async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    if (!name) throw new Error("El nombre de la actividad esta indefinido");
+
+    const activity = await getActivityByName(name);
+    if (!activity)
+      throw new Error(
+        `No se encontro ninguna actividad con el nombre ${name} en la BDD.`
+      );
+
+    res.status(200).json(activity);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.get("/getActivitiesByCountryName", async (req, res) => {
+  const { countryName } = req.query;
+
+  try {
+    if (!countryName) throw new Error("El nombre del pais esta indefinido.");
+
+    const activities = await getActivitiesByCountryName(countryName);
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.get("/getActivitiesByCountryId/:countryId", async (req, res) => {
+  const { countryId } = req.params;
+
+  try {
+    if (!countryId) throw new Error("El id del pais esta indefinido.");
+
+    const activities = await getActivitiesByCountryId(countryId);
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.post("/", async (req, res) => {
+  const { countriesId, name, difficulty, duration, season } = req.body;
+
+  try {
+    console.log("estoy en /activities");
+    if (
+      ![countriesId.length, name, difficulty, duration, season].every(Boolean)
+    ) {
+      throw new Error("Datos incompletos.");
     }
+
+    const touristActivity = await createActivity(
+      name,
+      difficulty,
+      duration,
+      season,
+      countriesId
+    );
+    if (!touristActivity)
+      throw new Error("Error al crear la actividad turistica.");
+
+    res.status(200).json(touristActivity);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+activityRouter.put("/update/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!id) throw new Error("El id de la actividad esta indefinido.");
+
+    const activityUpdated = await updateActivityById(id);
+    if (!activityUpdated)
+      throw new Error(
+        `No se encuentra una actividad con el id ${id} en la BDD.`
+      );
+
+    res.status(200).json(activityUpdated);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+activityRouter.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!id) throw new Error("El id de la actividad esta indefinido.");
+
+    const activityDeleted = await deleteActivityById(id);
+    if (!activityDeleted)
+      throw new Error(
+        `No se elimino ninguna actividad con el id ${id} en la BDD.`
+      );
+
+    res.status(200).json(activityDeleted);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = activityRouter;
