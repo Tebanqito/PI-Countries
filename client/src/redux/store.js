2fae01f6227c68c228f018c485b437237b3fe2bf
Tebanqito <yemesesteban@gmail.com>
2023-03-16 19:22:46 -0300

    store con countriesReducer utilizando redux-toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,20 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
-import { reducer } from "./reducer";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunkMiddleware from "redux-thunk";
+// import { reducer } from "./reducer";
 
-const composeEnahncer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnahncer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-    reducer,
-    composeEnahncer(applyMiddleware(thunkMiddleware))
-);
+// const store = createStore(
+//     reducer,
+//     composeEnahncer(applyMiddleware(thunkMiddleware))
+// );
+import { configureStore } from "@reduxjs/toolkit";
+import countriesReducer from "./reducers/countriesReducer";
+
+const store = configureStore({
+  reducer: {
+    countries: countriesReducer,
+  },
+});
 
 export default store;
