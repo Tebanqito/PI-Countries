af69b0236581aaf7f7526b7d95f2a9679c64c703
Tebanqito <yemesesteban@gmail.com>
2023-03-16 23:53:39 -0300

    codigo de activityReducer con formato

--- client/src/redux/reducers/activityReducer.js
+++ client/src/redux/reducers/activityReducer.js
@@ -1,115 +1,115 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
-    addActivity,
-    getActivities,
-    getActivitiesByCountryId,
-    getActivitiesByCountryName,
-    getActivitiesByDifficulty,
-    getActivitiesByName,
-    getActivitiesBySeason,
-    getActivityById,
+  addActivity,
+  getActivities,
+  getActivitiesByCountryId,
+  getActivitiesByCountryName,
+  getActivitiesByDifficulty,
+  getActivitiesByName,
+  getActivitiesBySeason,
+  getActivityById,
 } from "../actions/activityActions";
 
 const activitySlice = createSlice({
-    name: "activities",
-    initialState: {
-        detail: {},
-        list: [],
-        status: "idle",
-        error: null,
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(addActivity.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(addActivity.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.detail = action.payload;
-            })
-            .addCase(addActivity.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivities.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivities.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivities.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivityById.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivityById.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.detail = action.payload;
-            })
-            .addCase(getActivityById.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivitiesByCountryId.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivitiesByCountryId.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivitiesByCountryId.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivitiesByCountryName.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivitiesByCountryName.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivitiesByCountryName.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivitiesByDifficulty.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivitiesByDifficulty.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivitiesByDifficulty.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivitiesByName.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivitiesByName.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivitiesByName.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            })
-            .addCase(getActivitiesBySeason.pending, (state, action) => {
-                state.status = "loading";
-            })
-            .addCase(getActivitiesBySeason.fulfilled, (state, action) => {
-                state.status = "succeeded";
-                state.list = action.payload;
-            })
-            .addCase(getActivitiesBySeason.rejected, (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            });
-    }
+  name: "activities",
+  initialState: {
+    detail: {},
+    list: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addActivity.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(addActivity.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.detail = action.payload;
+      })
+      .addCase(addActivity.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivities.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivities.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivities.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivityById.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivityById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.detail = action.payload;
+      })
+      .addCase(getActivityById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivitiesByCountryId.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivitiesByCountryId.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivitiesByCountryId.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivitiesByCountryName.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivitiesByCountryName.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivitiesByCountryName.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivitiesByDifficulty.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivitiesByDifficulty.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivitiesByDifficulty.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivitiesByName.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivitiesByName.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivitiesByName.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(getActivitiesBySeason.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getActivitiesBySeason.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.list = action.payload;
+      })
+      .addCase(getActivitiesBySeason.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
 });
 
 export default activitySlice.reducer;
