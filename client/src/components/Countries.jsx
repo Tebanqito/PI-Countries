45351dc92e87678ef9be846e4ba17e5f23866d46
Tebanqito <yemesesteban@gmail.com>
2023-03-24 00:46:32 -0300

    setRenderActivity en Countries

--- client/src/components/Countries.jsx
+++ client/src/components/Countries.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
     CardContainer,
@@ -8,10 +8,12 @@ import {
     DetailButton
 } from "../styles/styles";
 import { unlinkCountry, linkCountry } from "../redux/actions/activityActions";
+import { getCountries } from "../redux/actions/countriesActions";
 
 const Countries = (props) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [renderCountries, setRenderCountries] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
 
     const countriesPerPage = currentPage === 1 ? 9 : 10;
@@ -23,12 +25,24 @@ const Countries = (props) => {
     const numCountries = props.countries.length;
     const finalPage = Math.ceil((numCountries - 9) / 10) + 1;
 
+    useEffect(() => {
+        if (!props.setRenderActivity) dispatch(getCountries());
+    }, [renderCountries]);
+
     const countriesToShow = countries.map(country => {
         return <li key={country?.id}>
             <CardContainer>
                 <DetailButton onClick={() => navigate(`/country/${country?.id}`)}>DETAIL</DetailButton>
-                {props.admin && <button onClick={() => dispatch(unlinkCountry({countryId: country?.id, activityId: props.id}))}>UNLINK</button>}
-                {props.dashboard && <button onClick={() => dispatch(linkCountry({countryId: country?.id, activityId: props.id}))}>LINK</button>}
+                {props.adminUnlinkCountry && <button onClick={() => {
+                    dispatch(unlinkCountry({ countryId: country?.id, activityId: props.activityId }));
+                    if (setRenderCountries) setRenderCountries(!renderCountries);
+                    if (setRenderActivity) setRenderActivity(!renderActivity);
+                }}>UNLINK</button>}
+                {props.adminlinkCountry && <button onClick={() => {
+                    dispatch(linkCountry({ countryId: country?.id, activityId: props.activityId }));
+                    if (setRenderCountries) setRenderCountries(!renderCountries);
+                    if (props.setRenderActivity) props.setRenderActivity(!props.renderActivity);
+                }}>LINK</button>}
                 <p><b>NAME</b>: {country?.name}</p>
                 <p><b>CONTINENT</b>: {country?.continent}</p>
                 <CardImage src={country?.imgFlag} alt={`Flag of ${country?.name} `} />
