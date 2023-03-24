bc7649776b8db6e299c7712788d1003e01aec434
Tebanqito <yemesesteban@gmail.com>
2023-03-24 00:47:40 -0300

    renderizando Countries de la actividad

--- client/src/components/ActivityDetail.jsx
+++ client/src/components/ActivityDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getActivityById } from "../redux/actions/activityActions";
 import {
@@ -12,21 +12,28 @@ import Countries from "./Countries";
 const ActivityDetail = ({ id }) => {
     const dispatch = useDispatch();
     const activity = useSelector(state => state.activities.detail);
+    const [renderActivity, setRenderActivity] = useState(true);
 
     useEffect(() => {
         dispatch(getActivityById(id));
-    }, []);
+    }, [renderActivity]);
 
     return (
         <div>
             <div>
-                    <ActivityName>NAME: {activity.name}</ActivityName>
-                    <ActivityDifficulty>DIFFICULTY: {activity.difficulty}</ActivityDifficulty>
-                    <ActivitySeason>DIFFICULTY: {activity.season}</ActivitySeason>
-                    <ActivityDuration>DIFFICULTY: {activity.duration}</ActivityDuration>
+                <ActivityName>NAME: {activity.name}</ActivityName>
+                <ActivityDifficulty>DIFFICULTY: {activity.difficulty}</ActivityDifficulty>
+                <ActivitySeason>DIFFICULTY: {activity.season}</ActivitySeason>
+                <ActivityDuration>DIFFICULTY: {activity.duration}</ActivityDuration>
             </div>
 
-            <Countries countries={activity?.countries} adminUnlinkCountry={true}/>
+            <Countries
+                countries={activity?.countries}
+                adminUnlinkCountry={true}
+                setRenderActivity={setRenderActivity}
+                renderActivity={renderActivity}
+                activityId={activity?.id}
+            />
         </div>
     );
 };
