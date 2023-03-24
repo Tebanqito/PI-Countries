4bba3f914fdad93ce34f9d1b32923df2466b1112
Tebanqito <yemesesteban@gmail.com>
2023-03-23 23:27:38 -0300

    Route /dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage';
 import Home from './components/Home';
 import CountryDetail from './components/CountryDetail';
 import Form from './components/Form';
+import Dashboard from './components/Dashboard';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/country/:countryId" element={<CountryDetail />} />
         <Route path="/home" element={<Home />} />
         <Route path="/activity" element={<Form />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
