8ed2bee0117d6666c280ffba20a90566c6ae641c
Tebanqito <yemesesteban@gmail.com>
2023-04-06 18:48:40 -0300

    dashboard renderizando Activities

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getActivities } from "../redux/actions/activityActions";
+import Activities from "./Activities";
+
 const Dashboard = () => {
+    const dispatch = useDispatch();
+    const activities = useSelector(state => state.activities.list);
+
+    useEffect(() => {
+        dispatch(getActivities());
+    }, []);
 
     return (
         <>
+            <Activities activities={activities} />
         </>
     );
 };
